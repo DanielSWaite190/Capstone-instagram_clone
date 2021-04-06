b5f4edaa69093b6eb007cebf861d244395db9b26
Daniel S. Waite <danielwaite190@gmail.com>
2021-04-06 13:01:20 -0700

    Replaced secrets.py file with .env file. Added dotenv package to project.

--- instagram_clone_project/settings.py
+++ instagram_clone_project/settings.py
@@ -1,5 +1,6 @@
 import os
-from secrets import *
+from dotenv import load_dotenv
+load_dotenv()
 """
 Django settings for instagram_clone_project project.
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
