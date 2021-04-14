54ff556928f19ec8385defc95a8ef407279c9ceb
Ana Ruiz Tovar <anapowa@gmail.com>
2021-04-13 21:17:08 -0400

    Saved changes from merged conflicts.

--- post_app/views.py
+++ post_app/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.views.generic import CreateView  # ListView  # new
 from django.urls import reverse_lazy  # new
 from post_app.models import ImageModel  # new
@@ -6,19 +5,12 @@ from post_app.forms import ImageModelForm  # new
 from django.shortcuts import HttpResponseRedirect  # A
 from django.contrib.auth.decorators import login_required  # A
 
-=======
-from django.views.generic import ListView, CreateView # new
-from django.urls import reverse_lazy # new
-from post_app.models import ImageModel # new
-from post_app.forms import ImageModelForm # new
->>>>>>> 479dc0d1196ad9795d45af1bf9c03096690bea92
 
 class CreatePostView(CreateView):  # new
     model = ImageModel
     form_class = ImageModelForm
     template_name = 'post.html'
     success_url = reverse_lazy('home_feed')
-<<<<<<< HEAD
 
 
 # A
@@ -41,5 +33,3 @@ def unlike_photo_view(request, post_id):
     current_user.likes.remove(post)
     current_user.save()
     return HttpResponseRedirect(redirect)
-=======
->>>>>>> 479dc0d1196ad9795d45af1bf9c03096690bea92
